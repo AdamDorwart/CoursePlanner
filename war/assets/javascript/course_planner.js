2148435f558ce79475dc32ec07f45907985d4b0b
Dylan Knutson <tcdknutson@gmail.com>
2013-11-02 17:44:56 -0700

    stylistic fixes

--- war/assets/javascript/course_planner.js
+++ war/assets/javascript/course_planner.js
@@ -4,4 +4,12 @@
     angular.module("CoursePlannerApp", ["lvl.directives.dragdrop"]);
   window.coursePlannerApp = coursePlannerApp;
 
+  coursePlannerApp.
+    filter('htmlToPlaintext', function() {
+      return function(text) {
+        return String(text).replace(/<(?:.|\n)*?>/gm, '');
+      };
+    }
+  );
+
 })();
